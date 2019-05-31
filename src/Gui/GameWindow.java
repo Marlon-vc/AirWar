27634ccdb8dec5dd72f302a61952922d50cd9cdd
Hazel <hazel.martilo@gmail.com>
2019-05-31 17:58:08 -0600

    Se carga la imagen mapa de fondo

--- src/Gui/GameWindow.java
+++ src/Gui/GameWindow.java
@@ -27,6 +27,28 @@ public class GameWindow extends Application {
         this.controller.setGameWindow(this);
     }
 
+    private void mapBackground(){
+        /**
+         * metodo para cargar la imagen mapa de fondo
+         */
+    }
+        mapContainer = new BorderPane();
+        Image mapImage = imageLoader(cws + "/res/images/map.jpg");
+    }
+
+    private Image imageLoader(String path){
+        /**
+         * metodo para obtener la direccion de la imagen
+         */
+        try{
+            FileInputStream i = new FileInputStream(path);
+            return new Image(i);
+        }catch (FileNotFoundException e){
+            System.out.println("Couldn't load images!");
+        }
+        System.out.println("Could not find " + path);
+        return null;
+
     public void show() {
         launch(GameWindow.class);
     }
