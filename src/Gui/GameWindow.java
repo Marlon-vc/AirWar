ed5c40336fe2e115658fc30f7c9a45386ba39448
Hazel <hazel.martilo@gmail.com>
2019-05-31 23:07:20 -0600

    Cambios Image
    cambios debidos a la biblioteca de javafx

--- src/Gui/GameWindow.java
+++ src/Gui/GameWindow.java
@@ -3,11 +3,18 @@ package Gui;
 import Logic.Controller;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class GameWindow extends Application {
     private Controller controller;
+    private String cws = System.getProperty("user.dir");
+
 
     @Override
     public void start(Stage stage) {
@@ -27,29 +34,30 @@ public class GameWindow extends Application {
         this.controller.setGameWindow(this);
     }
 
-    private void mapBackground(){
+    private void mapBackground() {
         /**
          * metodo para cargar la imagen mapa de fondo
          */
-    }
-        mapContainer = new BorderPane();
+        BorderPane mapContainer = new BorderPane();
         Image mapImage = imageLoader(cws + "/res/images/map.jpg");
     }
 
-    private Image imageLoader(String path){
+    private javafx.scene.image.Image imageLoader(String path){
+//    Image imageLoader(String path) {
         /**
          * metodo para obtener la direccion de la imagen
          */
-        try{
+        try {
             FileInputStream i = new FileInputStream(path);
-            return new Image(i);
-        }catch (FileNotFoundException e){
+            return new javafx.scene.image.Image(i);
+        } catch (FileNotFoundException e) {
             System.out.println("Couldn't load images!");
         }
         System.out.println("Could not find " + path);
         return null;
-
-    public void show() {
-        launch(GameWindow.class);
     }
+
+     public void show(){
+            launch(GameWindow.class);
+        }
 }
