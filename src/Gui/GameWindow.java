a4a98217600d10cf581fd4638b874ff14241c6d1
Hazel <hazel.martilo@gmail.com>
2019-06-03 19:58:40 -0600

    Background Image
    Metodo para colocar la imagen de fondo

--- src/Gui/GameWindow.java
+++ src/Gui/GameWindow.java
@@ -3,19 +3,36 @@ package Gui;
 import Logic.Controller;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class GameWindow extends Application {
     private Controller controller;
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws FileNotFoundException {
         initialize();
+        String root=System.getProperty("user.dir");
+        String rutaRlativa="/AirWar/res/images/map.jpg";
+        String url=root+rutaRlativa;
+        FileInputStream i = new FileInputStream(url);
+        ImageView addTokenImage = new ImageView(new Image(i));
+        //////////////////////////////////////////////////////
+
+        i = new FileInputStream("/home/hazel/Escritorio/airWar/AirWar/res/images/airport.png");
+        ImageView addTokenImage1 = new ImageView(new Image(i));
+        addTokenImage1.setX(50);
+        addTokenImage1.setY(50);
 
         BorderPane mainLayout = new BorderPane();
-
-        Scene scene = new Scene(mainLayout, 600, 400);
+        mainLayout.getChildren().addAll(addTokenImage,addTokenImage1);
+        Scene scene = new Scene(mainLayout, 1920, 1024);
         stage.setScene(scene);
         stage.setTitle("AirWar");
         stage.show();
