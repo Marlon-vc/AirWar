2dddbfb5e9d96f8fd98c60d38215f1610f694f23
Hazel <hazel.martilo@gmail.com>
2019-06-10 09:04:53 -0600

    Se crea clase Ruta

---
+++ src/Logic/Ruta.java
@@ -0,0 +1,40 @@
+package Logic;
+
+public class Ruta {
+    private double x1; //x1 y y1 es el punto de origen
+    private double y1;
+    private double x2; //x2 y y2 es el punto de destino
+    private double y2;
+
+    public double getX1() {
+        return x1;
+    }
+
+    public void setX1(double x1) {
+        this.x1 = x1;
+    }
+
+    public double getY1() {
+        return y1;
+    }
+
+    public void setY1(double y1) {
+        this.y1 = y1;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public double getY2() {
+        return y2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
+}
