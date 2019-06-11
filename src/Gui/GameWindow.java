1044520434fff7f79935606839ede3811b59c6cb
Marlon Vega <mvegach2013@gmail.com>
2019-06-10 18:03:32 -0600

    Cambiado el método para cargar imágenes, usar el método de Controller

--- src/Gui/GameWindow.java
+++ src/Gui/GameWindow.java
@@ -6,36 +6,31 @@ import Sprites.Airport;
 import Structures.LinkedList;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public class GameWindow extends Application {
     private Controller controller;
     private BorderPane mainLayout;
 
 @Override
-    public void start(Stage stage) throws FileNotFoundException {
+    public void start(Stage stage) {
         initialize();
-        String root=System.getProperty("user.dir");
-        String rutaRlativa="/AirWar/res/images/map.jpg";
-        String url=root+rutaRlativa;
-        FileInputStream i = new FileInputStream(url);
-        ImageView addTokenImage = new ImageView(new Image(i));
+
+        ImageView addTokenImage = new ImageView(Controller.loadImage("res/images/map.jpg"));
         //////////////////////////////////////////////////////
 
-        i = new FileInputStream("AirWar/res/images/airport21.png");
-        ImageView addTokenImage1 = new ImageView(new Image(i));
-        addTokenImage1.setX(50);
-        addTokenImage1.setY(50);
+        ImageView airportImg = new ImageView(Controller.loadImage("/res/images/airport2.png"));
+        airportImg.setFitWidth(25);
+        airportImg.setPreserveRatio(true);
+        airportImg.setX(50);
+        airportImg.setY(50);
+
 ////////////////////////////////////////////////////////////
-        Airport airport = new Airport(new Image(i), 200,200);
-        Airport airport1 = new Airport(new Image(i), 300,300);
-        Airport airport2 = new Airport(new Image(i), 400,400);
+        Airport airport = new Airport(Controller.loadImage("/res/images/airport2.png"), 200,200);
+        Airport airport1 = new Airport(Controller.loadImage("/res/images/airport2.png"), 300,300);
+        Airport airport2 = new Airport(Controller.loadImage("/res/images/airport2.png"), 400,400);
         LinkedList<Airport> listaAirport = new LinkedList<>();
         listaAirport.add(airport);
         listaAirport.add(airport1);
@@ -50,12 +45,14 @@ public class GameWindow extends Application {
 
 
         BorderPane mainLayout = new BorderPane();
-        mainLayout.getChildren().addAll(addTokenImage,addTokenImage1);
+        mainLayout.getChildren().addAll(addTokenImage,airportImg);
         Scene scene = new Scene(mainLayout, 1920, 1024);
         stage.setScene(scene);
         stage.setTitle("AirWar");
         stage.show();
-}
+
+        controller.load(100000, mainLayout);
+    }
 
     private void initialize() {
         this.controller = Controller.getInstance();
