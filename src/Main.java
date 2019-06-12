4659c68c1f74a4967bc0a8adec21206b2d4ff0fe
paola0298 <pvillegasch@gmail.com>
2019-06-12 11:18:42 -0600

    Pruebas

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import Gui.GameWindow;
+import Gui.InitialWindow;
 
 public class Main {
 
     public static void main(String[] args) {
-        GameWindow game = new GameWindow();
+//        GameWindow game = new GameWindow();
+//        game.show();
+        InitialWindow game = new InitialWindow();
         game.show();
     }
 }
