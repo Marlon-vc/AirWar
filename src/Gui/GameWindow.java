14c07976e74891890a6dae7bd238928f5d2bae3c
paola0298 <pvillegasch@gmail.com>
2019-06-12 11:18:24 -0600

    Cambios menores

--- src/Gui/GameWindow.java
+++ src/Gui/GameWindow.java
@@ -1,24 +1,13 @@
 package Gui;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonBar;
-import javafx.scene.control.ButtonType;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class GameWindow extends Application {
 
     @Override
     public void start(Stage stage) {
-        BorderPane mainLayout = new BorderPane();
 
-        Scene scene = new Scene(mainLayout, 600, 400);
-        stage.setScene(scene);
-        stage.setTitle("AirWar");
-        stage.show();
     }
 
     public void show() {
