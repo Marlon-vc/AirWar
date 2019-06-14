26222e54383eef0c26acfc2408fa6fd2d18c6847
Marlon Vega <mvegach2013@gmail.com>
2019-06-14 05:35:27 -0600

    Añadido hilo del juego

--- src/Logic/Controller.java
+++ src/Logic/Controller.java
@@ -2,6 +2,7 @@ package Logic;
 
 import Gui.GameWindow;
 import Sprites.Airport;
+import Sprites.Plane;
 import Structures.AdjacencyMatrix;
 import Structures.LinkedList;
 import javafx.application.Platform;
@@ -16,16 +17,23 @@ public class Controller {
 
     private static Controller instance;
     private GameWindow gameWindow;
-
     private LinkedList<Airport> airportList;
-    private AdjacencyMatrix airportRoutes;
+    private LinkedList<Plane> planesList;
+    private AdjacencyMatrix graph;
 
     private Image airportImage;
+    private Image planeImage;
+    private boolean isGameRunning;
 
     private Controller() {
         airportImage = loadImage("/res/images/airport2.png");
+        planeImage = loadImage("/res/images/plane.png");
     }
 
+    /**
+     * Método que devuelve la instancia de controller.
+     * @return Instancia de controller.
+     */
     public static Controller getInstance() {
         if (instance == null) {
             instance = new Controller();
@@ -33,12 +41,18 @@ public class Controller {
         return instance;
     }
 
-    public void load(int airportCount, Pane container) {
-        System.out.println("Loading..");
+    /**
+     * Método que inicializa los componentes del juego.
+     * @param airportCount Cantidad de aeropuertos a generar.
+     */
+    public void load(int airportCount) {
+        GameWindow.show();
         Thread loadThread = new Thread(() -> {
             generateAirports(airportCount);
-            this.airportRoutes = new AdjacencyMatrix(airportList);
-            renderAirports(container);
+            this.graph = new AdjacencyMatrix(airportList);
+            renderAirports(gameWindow.getMainContainer());
+            System.out.println("Starting game thread..");
+            startGameThread(gameWindow.getMainContainer());
             System.out.println("..done");
         });
 
@@ -46,23 +60,99 @@ public class Controller {
         loadThread.start();
     }
 
+    /**
+     * Método que inicia el hilo principal del juego.
+     * @param gamePane Contenedor principal de la interfaz.
+     */
+    private void startGameThread(Pane gamePane) {
+        isGameRunning = true;
+
+        Thread gameThread = new Thread(() -> {
+            int frames = 0;
+            double unprocessedSecs = 0;
+            long prevTime = System.nanoTime();
+            double secsPerTick = 1 / 60.0;
+            int tickCount = 0;
+            while(isGameRunning) {
+                long currentTime = System.nanoTime();
+                long passedTime = currentTime - prevTime;
+                prevTime = currentTime;
+                unprocessedSecs += passedTime /  1000000000.0;
+                while (unprocessedSecs > secsPerTick) {
+                    updateGame(unprocessedSecs);
+                    unprocessedSecs -= secsPerTick;
+                    tickCount++;
+                    if (tickCount % 60 == 0) {
+                        System.out.println(frames + " FPS");
+                        prevTime += 1000;
+                        frames = 0;
+                    }
+                }
+                render(gamePane);
+                frames++;
+            }
+        });
+        gameThread.setDaemon(true);
+        gameThread.start();
+    }
+
+    /**
+     * Método para actualizar el estado del juego.
+     * @param secs Tiempo transcurrido.
+     */
+    private void updateGame(double secs) {
+        //TODO actualizar el estado del juego
+        for (int i=0; i<airportList.getSize(); i++) {
+            Airport airport = airportList.get(i);
+            if (airport.hasTimeLeft()) {
+                if (airport.isEmpty()) {
+                    Plane plane = new Plane(planeImage, airport.getPosX(), airport.getPosY());
+                    //TODO inicializar el movimiento del avión.
+                } else {
+                    //TODO lanzar el avión en espera
+                }
+            } else {
+                //TODO decrementar el tiempo restante.
+            }
+        }
+    }
+
+    /**
+     * Método encargado de renderizar los componentes.
+     * @param gamePane Contenedor principal de la intefaz.
+     */
+    private void render(Pane gamePane) {
+        //TODO renderizar los cambios en la interfaz.
+        for (int i=0; i<planesList.getSize(); i++) {
+            planesList.get(i).updatePos();
+        }
+    }
+
+    /**
+     * Método encargado de generar los aeropuertos con posiciones aleatorias.
+     * @param count Cantidad de aeropuertos a generar.
+     */
     private void generateAirports(int count) {
+
+        System.out.println("Generating airports..");
         airportList = new LinkedList<>();
 
         for (int i=0; i<count; i++) {
             Airport airport = new Airport(airportImage, i,
                     ThreadLocalRandom.current().nextDouble(0, 1281),
                     ThreadLocalRandom.current().nextDouble(0, 721));
+            airport.setTime(ThreadLocalRandom.current().nextDouble(0, 10));
             airport.setSize(25);
-
-            //TODO verificar si la posición (x, y) corresponde a tierra o no, para asignar la imagen respectiva.
-
             airportList.add(airport);
         }
     }
 
-    public void renderAirports(Pane container) {
-        //TODO mostrar los aeropuertos en la ventana principal.
+    /**
+     * Éste método se encarga de mostrar los aeropuertos generados en la interfaz.
+     * @param container Contenedor principal de la interfaz.
+     */
+    private void renderAirports(Pane container) {
+        System.out.println("Rendering airports..");
         for (int i=0; i<airportList.getSize(); i++) {
             int finalI = i;
             Platform.runLater(() -> container.getChildren().add(airportList.get(finalI).getImage()));
@@ -121,6 +211,10 @@ public class Controller {
         return listaRutas;
     }
 
+    /**
+     * Método que guarda la referencia de la interfaz principal en una variable de clase.
+     * @param gameWindow Interfaz principal.
+     */
     public void setGameWindow(GameWindow gameWindow) {
         this.gameWindow = gameWindow;
     }
