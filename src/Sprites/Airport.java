e95891f2ec7466e7dc00be0610a1c3cfbf9479db
Marlon Vega <mvegach2013@gmail.com>
2019-06-14 05:30:43 -0600

    Optimizados las sprites

--- src/Sprites/Airport.java
+++ src/Sprites/Airport.java
@@ -1,6 +1,6 @@
 package Sprites;
 
-import Structures.LinkedList;
+import Structures.Queue;
 import javafx.scene.control.Tooltip;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
@@ -9,26 +9,35 @@ import javafx.scene.paint.Color;
 public class Airport extends Sprite {
 
     private int id;
-    private LinkedList<Plane> planes;
+    private double time;
+    private Queue<Plane> planeQueue;
 
     public Airport(Image spriteImage, int id, double posX, double posY) {
         super(spriteImage, posX, posY);
         this.id = id;
-        this.planes = new LinkedList<>();
-        init();
-    }
+        this.planeQueue = new Queue<>();
 
-    private void init() {
         Tooltip tooltip = new Tooltip();
         tooltip.setText("Airport " + id + "\nPos X: " + posX + "\nPos Y: " + posY);
         Tooltip.install(image, tooltip);
-
         image.setOnMouseEntered(mouseEvent -> image.setEffect(new DropShadow(5, Color.LIGHTGRAY)));
         image.setOnMouseExited(mouseEvent -> image.setEffect(null));
     }
 
-    public int getId() {
-        return this.id;
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public double getTime() {
+        return this.time;
+    }
+
+    public boolean isEmpty() {
+        return (planeQueue.getSize() == 0);
+    }
+
+    public void addPlane(Plane plane) {
+        planeQueue.enqueue(plane);
     }
 
 }
--- src/Sprites/Plane.java
+++ src/Sprites/Plane.java
@@ -1,23 +1,43 @@
 package Sprites;
 
 import Structures.LinkedList;
+import Structures.Queue;
 import javafx.scene.image.Image;
 
+/*TODO:
+       Para la trayectoria del avión utilizar una función lineal (y=mx+b).
+       Con la posición (x, y) del destino y la actual del avión calcular el criterio de la función.
+       Luego calcular la distancia en X entre los puntos, y dividirla en partes (unas 10 o más).
+       Para cada parte (posición en X) calcular la posición respectiva en Y, utilizando la función lineal.
+       Mover el avión repitiendo los pasos, sumando a la posición en X una parte.
+       .
+
+      TODO:
+       Para el ángulo de rotación del avión calcular el ángulo entre la recta que contiene al avión y al destino,
+       con respecto a la horizontal, y girar el avión esa cantidad de grados.
+       .
+       https://stackoverflow.com/questions/2676719/calculating-the-angle-between-the-line-defined-by-two-points
+    * */
 public class Plane extends Sprite {
 
-    private LinkedList<Airport> route;
     private double speed;
+    private Airport currentDestination;
+    private Queue<Airport> route;
+
+    private double m;
+    private double b;
 
     public Plane() {
         super();
         this.speed = 1;
     }
 
-    public Plane(Image planeImage) {
-        super(planeImage);
+    public Plane(Image planeImage, double posX, double posY) {
+        super(planeImage, posX, posY);
         this.speed = 1;
-        //TODO seleccionar aleatoriamente un aeropuerto para navegar y calcular la ruta más corta
+
     }
+
     public double getSpeed() {
         return speed;
     }
@@ -25,4 +45,9 @@ public class Plane extends Sprite {
     public void setSpeed(double speed) {
         this.speed = speed;
     }
+
+    public boolean isEnd() {
+        return (route.getSize() == 0);
+    }
+
 }
