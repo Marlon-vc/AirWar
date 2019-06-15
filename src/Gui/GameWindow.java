c22eadb2c87c920b884dffe49534557b8116d739
paola0298 <pvillegasch@gmail.com>
2019-06-14 23:53:46 -0600

    Agregado temporizador del juego

--- src/Gui/GameWindow.java
+++ src/Gui/GameWindow.java
@@ -1,61 +1,70 @@
 package Gui;
 
 import Logic.Controller;
-import Logic.Ruta;
-import Sprites.Airport;
-import Structures.LinkedList;
+import Structures.Timer;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class GameWindow extends Application {
     private Controller controller;
     private BorderPane mainLayout;
-
     private PixelReader pixelReader;
+    private String cwd = System.getProperty("user.dir");
+
 
     @Override
     public void start(Stage stage) {
-
-
         mainLayout = new BorderPane();
         initialize();
 
+        Image imageColor = new Image("file://" + cwd +"/res/images/map1.jpg", 1280, 720, false, false);
+        pixelReader = imageColor.getPixelReader();
         Image mapImg = Controller.loadImage("/res/images/map.jpg");
         ImageView mapIV = new ImageView(mapImg);
         mapIV.setFitWidth(1280);
         mapIV.setFitHeight(720);
 
-        Image imageColor = new Image("file://" + System.getProperty("user.dir")+"/res/images/map1.jpg", 1280, 720, false, false);
-        pixelReader = imageColor.getPixelReader();
+        //timer
 
+        HBox timerBox = new HBox();
+        timerBox.setAlignment(Pos.CENTER);
+        timerBox.setSpacing(10);
+        timerBox.setPadding(new Insets(10));
 
-        mapIV.setOnMouseClicked(mouseEvent -> {
-            Color color = pixelReader.getColor((int) mouseEvent.getX(), (int) mouseEvent.getY());
-            int colorInt = pixelReader.getArgb((int) mouseEvent.getX(), (int) mouseEvent.getY());
-            int red = (colorInt >> 16) & 0xff;
-            int green = (colorInt >> 8) & 0xff;
-            int blue = colorInt & 0xff;
+        Label timerLabel = new Label();
+        System.out.println("Player name " + this.controller.getPlayerName());
+        Label playerName = new Label("Jugador: " + this.controller.getPlayerName() + " -  Tiempo restante:");
 
-//            System.out.println("Color at click:\nRed: " + red + "\nGreen: " + green + "\n Blue: " + blue);
-            System.out.println("Color at click: " + colorInt);
-        });
+        Timer timer = new Timer(60*5, timerLabel);
+        timer.startTimer();
+
+        timerBox.getChildren().addAll(playerName, timerLabel);
+
+        //colocar estilo
+        timerLabel.setId("game-label");
+        playerName.setId("game-label");
+        timerBox.setId("game-container");
+
 
         mainLayout.getChildren().addAll(mapIV);
+        mainLayout.setTop(timerBox);
+
         Scene scene = new Scene(mainLayout, 1280, 720);
         stage.setResizable(false);
+        scene.getStylesheets().add(("file:///" + cwd + "/res/Style/style.css").replace(" ", "%20"));
         stage.setScene(scene);
         stage.setTitle("AirWar");
         stage.show();
-
-
     }
 
     public Pane getMainContainer() {
@@ -69,7 +78,6 @@ public class GameWindow extends Application {
     private void initialize() {
         this.controller = Controller.getInstance();
         this.controller.setGameWindow(this);
-
     }
 
     public static void show() {
