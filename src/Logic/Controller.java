497957883b6942e780c07c024c0956e05c09ff6e
Brayan-00 <brayanfernando.10@gmail.com>
2019-06-16 01:40:52 -0600

    Se mueve la bateria pero ahora con problemas

--- src/Logic/Controller.java
+++ src/Logic/Controller.java
@@ -421,8 +421,6 @@ public class Controller {
         });
     }
 
-
-
     /**
      * Método que guarda la referencia de la interfaz principal en una variable de clase.
      * @param gameWindow Interfaz principal.
